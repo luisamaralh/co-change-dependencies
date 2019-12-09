6decc258330d98be905d77996fddaf99975ae506
Otto von Sperling <otto@vonsperling.com.br>
2019-12-09 08:56:03 -0300

    FIXED import error

--- setup_env_run_notebook.sh
+++ setup_env_run_notebook.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ ! -d ./pyenv/ ];
+if [ ! -d ./.venv/ ];
 then
-    python3 -m venv ./pyenv/ && . ./pyenv/bin/activate
+    python3 -m venv ./.venv/ && . ./.venv/bin/activate
     python -m pip install pip --upgrade
     python -m pip install -r ./requirements.txt    
 
 else
-    . ./pyenv/bin/activate
+    . ./.venv/bin/activate
     python -m pip install -r ./requirements.txt
 fi
 
